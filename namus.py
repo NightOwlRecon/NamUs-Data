9d9c5bd42cf047694d84b1fb341c784e4bf15fb8
Nick Price <nick@spun.io>
2024-08-11 12:33:28 -0700

    reformat to allow diffs and other text tools to work with output

--- namus.py
+++ namus.py
@@ -6,7 +6,7 @@ import requests
 
 
 def load_stored_cases():
-    with open("cases.json", "r") as f:
+    with open("output/namus-20240811.json", "r") as f:
         cases = json.load(f)
     return cases
 
@@ -14,8 +14,28 @@ def load_stored_cases():
 def save_cases(cases):
     # this is a very naive way to save cases - we should probably use a database
     date = datetime.datetime.now().strftime("%Y%m%d")
-    with open(f"output/namus-{date}.json", "w") as f:
-        json.dump(cases, f)
+
+    # sort by case ID first
+    cases.sort(key=lambda x: x["id"])
+
+    with open(f"output/namus-{date}.json2", "w") as f:
+        # handling this manually to keep a one-case-per-line format for easy diffing while still maintaining a valid
+        # JSON object (versus jsonlines which would add another dependency for using the data)
+
+        # open the JSON array
+        f.write("[\n")
+
+        for case in cases:
+            # add a tab before each line to make it pretty
+            f.write("\t")
+            json.dump(case, f)
+            # skip the final comma on the last entry to maintain a valid JSON object
+            if case == cases[-1]:
+                f.write("\n")
+            else:
+                f.write(",\n")
+        # close the JSON array
+        f.write("]\n")
 
 
 def get_states():
@@ -65,7 +85,7 @@ def main():
         print(f"Found {len(ids)} cases in {state}")
         case_ids.extend(ids)
 
-    print(f"Found {len(case_ids)} total cases")
+    print(f"Found {len(case_ids)} total ")
 
     for i in range(len(case_ids)):
         while True:
